70d5a3573648318f3bcf8f7cf7d84782e9afa7df
Pavel Ivanov <esputnix@gmail.com>
2019-09-12 21:34:11 -0700

    added modules

---
+++ README.md
@@ -0,0 +1,8 @@
+# NodeJS Express
+
+```
+npm install @okta/oidc-middleware
+npm install express-session --save
+npm install --save-exact express@4.17.1 cors@2.8.5
+
+```
---
+++ events.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+function get_data() {
+  return {'one': 1, 'two': 2, 'three': 3}
+}
+
+function createRouter() {
+  const router = express.Router();
+  const owner = '';
+
+  router.get('/event', function (req, res, next) {
+      get_data( (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });
+
+  router.put('/event/:id', function (req, res, next) {
+    get_data( (error) => {
+        if (error) {
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json({status: 'ok'});
+        }
+      }
+    );
+  });
+
+  router.delete('/event/:id', function (req, res, next) {
+    get_data( (error) => {
+        if (error) {
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json({status: 'ok'});
+        }
+      }
+    );
+  });
+
+  return router;
+}
+
+module.exports = createRouter;
